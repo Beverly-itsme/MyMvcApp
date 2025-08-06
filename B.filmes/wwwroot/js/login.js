c860161ff01baf9636535e9ee4f186c2948156f9
Beverly-itsme <zhuakibeverly7@gmail.com>
2025-08-06 23:53:36 +0200

    css and js add

---
+++ B.filmes/wwwroot/js/login.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (e) => {
+        const email = form.email.value.trim();
+        const senha = form.senha.value.trim();
+
+        if (!email || !senha) {
+            alert("Por favor, preencha todos os campos!");
+            e.preventDefault();
+        }
+    });
+});
